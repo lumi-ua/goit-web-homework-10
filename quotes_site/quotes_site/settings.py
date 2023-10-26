518db72077d1c30d857f567d6ffcfd14d1195db8
lumi-ua <130182058+lumi-ua@users.noreply.github.com>
2023-10-26 15:43:41 +0300

    Update settings.py

--- quotes_site/quotes_site/settings.py
+++ quotes_site/quotes_site/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'app_quotes',
         'USER': 'postgres',
-        'PASSWORD': '77766',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
